4aa4f3483b1c4ee933be1d8cbbdd06d2239eef87
Badrinath Danave <badrinathdanave@gmail.com>
2023-04-27 17:25:19 +0530

    initial commit

---
+++ src/main/java/com/springboot/data/jpa/springbootdatajpahandson/SpringbootDataJpaHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.data.jpa.springbootdatajpahandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataJpaHandsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDataJpaHandsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/data/jpa/springbootdatajpahandson/SpringbootDataJpaHandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.data.jpa.springbootdatajpahandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataJpaHandsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
